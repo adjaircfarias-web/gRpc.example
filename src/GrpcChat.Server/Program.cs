2cb29d3f55ecfadd33a6d736b51fc79f7bdd596f
Adjair Farias <adjaircfarias@gmail.com>
2026-01-22 16:37:14 -0300

    refact: bug

--- src/GrpcChat.Server/Program.cs
+++ src/GrpcChat.Server/Program.cs
@@ -1,8 +1,16 @@
 using GrpcChat.Server.Extensions;
 using GrpcChat.Server.Interceptors;
 using GrpcChat.Server.Services;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// Configure Kestrel to use HTTP/2 (required for gRPC)
+builder.WebHost.ConfigureKestrel(options =>
+{
+    // Setup HTTP/2 endpoint without TLS for gRPC
+    options.ListenAnyIP(5001, o => o.Protocols = HttpProtocols.Http2);
+});
 
 // Add gRPC services with interceptor
 builder.Services.AddGrpc(options =>
