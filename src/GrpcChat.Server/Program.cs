5b8fb38ff9ac32f797505184d5a8eff99f759fd0
Adjair Farias <adjair.farias@pxbank.com.br>
2026-01-22 10:01:34 -0300

    feat: US-003

--- src/GrpcChat.Server/Program.cs
+++ src/GrpcChat.Server/Program.cs
@@ -1,6 +1,18 @@
+using GrpcChat.Server.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add gRPC services
+builder.Services.AddGrpc(options =>
+{
+    options.EnableDetailedErrors = true;
+});
+
+// Add Redis services
+builder.Services.AddRedisServices(builder.Configuration);
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/", () => "gRPC Chat Server is running");
 
 app.Run();
